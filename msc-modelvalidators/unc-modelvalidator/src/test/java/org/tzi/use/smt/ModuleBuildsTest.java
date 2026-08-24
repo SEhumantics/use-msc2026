c01878faa75d321ec01b7154cb06dbe9aad52b3b
Trong-Nghia Be <benghia1st@gmail.com>
2026-08-24 22:39:05 +0700

    feat(unc): convert unc-modelvalidator to a jar module with use-core on the classpath

---
+++ msc-modelvalidators/unc-modelvalidator/src/test/java/org/tzi/use/smt/ModuleBuildsTest.java
@@ -0,0 +1,19 @@
+package org.tzi.use.smt;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.tzi.use.uml.mm.ModelFactory;
+
+/**
+ * Smoke test: the module compiles, JUnit runs, and use-core is genuinely on the classpath.
+ * If this fails, nothing else in the module can be trusted.
+ */
+public class ModuleBuildsTest {
+
+    @Test
+    public void useCoreIsOnTheClasspath() {
+        ModelFactory factory = new ModelFactory();
+        assertNotNull(factory.createModel("Smoke"));
+    }
+}
